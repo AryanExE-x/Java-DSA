7d8fbc44fd101024be896c2061ffdf8ee763b5f1
Aryan Prasad <aryanprasad1405@gmail.com>
2026-08-23 02:03:55 +0530

    DeleteNode in a Linked List

--- src/GFG/LinkedList_I/LC_237_DeleteNodeInLL.java
+++ src/GFG/LinkedList_I/LC_237_DeleteNodeInLL.java
@@ -1,4 +1,8 @@
 package GFG.LinkedList_I;
 
 public class LC_237_DeleteNodeInLL {
+    public void deleteNode(Node head){
+        head.val=head.next.val;
+        head.next=head.next.next;
+    }
 }
