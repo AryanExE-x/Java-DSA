4dacfd6b69218c761dc65d48a19b457e2195df59
Aryan Prasad <aryanprasad1405@gmail.com>
2026-08-22 10:37:34 +0530

    Updated rotateLinkedList comments n made it more convenient for revision

--- src/GFG/LinkedList_I/LC_61_rotateList.java
+++ src/GFG/LinkedList_I/LC_61_rotateList.java
@@ -24,8 +24,8 @@ public class LC_61_rotateList {
             slow = slow.next;
             fast = fast.next;
         }
-        Node a = slow.next;
-        slow.next=null; //breaking apart the linkedlist into 2 subparts
+        Node a = slow.next; /*the point from where we need to break the ll*/
+        slow.next=null; /*breaking apart the linkedlist into 2 subparts*/
         Node tail = a; //store a in tail and move it using while till it ends @null
         while(tail.next!=null){ /*tail!=null pe tail will stop at null. so we cant connect to head*/
             tail=tail.next;
