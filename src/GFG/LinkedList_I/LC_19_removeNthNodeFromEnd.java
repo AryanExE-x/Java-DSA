55597e6aff4dff8d55e9a6c1d163a93b54f39a78
Aryan Prasad <aryanprasad1405@gmail.com>
2026-08-23 10:48:04 +0530

    Leetcode 19 - Delete Nth Node from the end of the list

--- src/GFG/LinkedList_I/LC_19_removeNthNodeFromEnd.java
+++ src/GFG/LinkedList_I/LC_19_removeNthNodeFromEnd.java
@@ -1,4 +1,22 @@
 package GFG.LinkedList_I;
 
 public class LC_19_removeNthNodeFromEnd {
+    public Node removeNthFromEnd(Node head, int n) {
+        Node fast = head;
+        Node slow = head;
+        for(int i=1;i<=n;i++){
+            fast=fast.next;
+        }
+
+        if(fast==null){
+            return head.next;
+        }
+
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
 }
