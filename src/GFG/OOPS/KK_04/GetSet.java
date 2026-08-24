a797c54a300a66fadd8ca76c2f6ecd2ce1b60f78
Aryan Prasad <aryanprasad1405@gmail.com>
2026-08-24 20:17:45 +0530

    override methods

--- src/GFG/OOPS/KK_04/GetSet.java
+++ src/GFG/OOPS/KK_04/GetSet.java
@@ -13,6 +13,31 @@ public class GetSet{
         this.num=num;
     }
 
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
     GetSet(int num, String name){
         this.num=num;
         this.name = name;
