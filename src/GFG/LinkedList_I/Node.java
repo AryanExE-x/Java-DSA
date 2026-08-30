28778656be913ccb365d3084357800b5698b2633
Aryan Prasad <aryanprasad1405@gmail.com>
2026-08-31 00:42:48 +0530

    Node next stores reference to next node

--- src/GFG/LinkedList_I/Node.java
+++ src/GFG/LinkedList_I/Node.java
@@ -2,7 +2,7 @@ package GFG.LinkedList_I;
 
 public class Node{
     int val;
-    Node next;
+    Node next; //next stores a reference to another Node.  eg-> a.next=b;
     Node(int val){
         this.val=val;
     }
